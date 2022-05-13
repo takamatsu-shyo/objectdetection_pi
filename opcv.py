a2ad0b927dfbc630fd52459803d36c5c6d809973
Sho Yano <takamatsu.shyo@gmail.com>
2022-05-13 23:21:10 +0300

    save frame

--- opcv.py
+++ opcv.py
@@ -1,42 +1,47 @@
 import cv2
 import numpy as np
-from queue import Queue
-from threading import Thread, Lock
 import time
 
 
 def capture_to_buffer():
     cap = cv2.VideoCapture("data/test.mp4")
     frame_buffer = []
-    
+    width = 0
+    height = 0
+
     try:
-        #while(1):
-        for i in range(30):
+        for i in range(5):
             print(".", end="", flush=True)
             ret, img = cap.read()
             if(ret):
                 frame_buffer.append(img)
+                width = img.shape[1]
+                height = img.shape[0]
             else:
                 break
     except Exception as e:
         print(e)
 
-    return frame_buffer
+    return frame_buffer, width, height
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+
+    with open("data/coco.names", 'r') as f:
+        classes = [line.strip() for line in f.readlines()]
+    label = str(classes[class_id])
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), (0, 128, 128), 5)
+    cv2.putText(img, label, (x-20, y-20), cv2.FONT_HERSHEY_PLAIN, 5, (0, 128, 128), 2)
 
 
 def main():
 
-    frame_buffer = capture_to_buffer()
-    print("frame buffer complete")
-
-    Width = 1280 
-    Height = 720
+    frame_buffer, Width, Height = capture_to_buffer()
+    print("frame buffer complete", Width, Height)
 
     #net = cv2.dnn.readNet("data/yolov3.weights", "data/yolov3.cfg")
     net = cv2.dnn.readNet("data/yolov3-tiny.weights", "data/yolov3-tiny.cfg")
 
-    layer_names = net.getLayerNames()
-    #output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
     output_layers = net.getUnconnectedOutLayersNames()
 
     scale = 0.00392
@@ -45,6 +50,8 @@ def main():
     frame_counter = 0
     start_time = time.time()
     fps = 0.0
+
+    save_frame_buffer = []
 
     for img in frame_buffer:
         frame_counter += 1
@@ -74,14 +81,31 @@ def main():
                     class_ids.append(class_id)
                     confidences.append(float(confidence))
                     boxes.append([x, y, w, h])
-                    print(class_ids, confidence,)
 
         indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
         fps = frame_counter * 1.0 / (time.time() - start_time)
+
+        for i in indices:
+            i = i[0]
+            box = boxes[i]
+            x = box[0]
+            y = box[1]
+            w = box[2]
+            h = box[3]
+            draw_prediction(img, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+        save_frame_buffer.append(img)
 
         print(".", end="", flush=True)
 
     print(fps, " fps")
+
+    for i in range(len(save_frame_buffer)):
+        filename = "out/" + str(i) + ".jpg"
+        cv2.imwrite(filename, save_frame_buffer[i])
+        print(".", end="", flush=True)
+
+    print("save frame complete")
 
 
 if __name__ == "__main__":
